b17d63fad8b1aa21557d7c02dc03e6a092fde671
dongjin0521 <84802@naver.com>
2024-06-04 20:41:06 +0900

    이동진 커밋

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -1,4 +1,8 @@
-const Tag = (name: string) => {
+interface tagprops {
+  name: string;
+}
+
+const Tag = ({ name }: tagprops) => {
   return (
     <div
       style={{
@@ -7,6 +11,7 @@ const Tag = (name: string) => {
         textAlign: "center",
         borderRadius: "1rem",
         fontSize: "1.4rem",
+        float: "left",
       }}
     >
       {name}
